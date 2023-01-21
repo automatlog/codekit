274ec56f95bd286ad40e71de431fbd7d0dddb686
Aman Yadav <72098281+automatlog@users.noreply.github.com>
2023-01-22 02:45:03 +0530

    Create scriptname.sh

---
+++ scriptname.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Add the Microsoft repository
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+
+# Update the package list and install the editor
+sudo apt-get update
+sudo apt-get install code
